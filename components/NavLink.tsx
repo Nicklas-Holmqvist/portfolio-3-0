6420c61c46933f4995ee8cf3f1b73984a3063dbe
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2022-08-19 22:46:19 +0200

    feat: create header with content

---
+++ components/NavLink.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import React from 'react';
+import styled from 'styled-components';
+
+export interface NavLink {
+  linkText: string;
+}
+
+export const NavLink: React.FC<NavLink> = ({ linkText }) => {
+  const linkPath: string = linkText.toLowerCase().replace(' ', '_');
+
+  return (
+    <Link href={`#${linkPath}`}>
+      <LinkText>{linkText}</LinkText>
+    </Link>
+  );
+};
+
+const LinkText = styled.a`
+  position: relative;
+  text-decoration: none;
+  color: #f0f0f0;
+  cursor: pointer;
+  &:after {
+    content: '';
+    position: absolute;
+    background-color: #f0f0f0;
+    width: 0;
+    height: 2px;
+    bottom: -4px;
+    left: 0;
+    transition: 0.2s;
+  }
+  &:hover:after {
+    width: 100%;
+  }
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,14 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+
 import { createGlobalStyle } from 'styled-components';
 
 import { Layout } from '../components/Layout';
+import { NavHeader } from '../components/NavHeader';
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Nicklas Holmqvist portfolio | Fotograf och utvecklare</title>
-        <meta
-          name="description"
-          content="En portfolio skapad av Nicklas Holmqvist som en frontend utvecklare för att visa fotografier ur projektet Glömd värld i marks härad."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <GlobalStyles />
-      <header>
-        {/* <Image src="" alt="" width="10" height="10" /> */}
-        <nav></nav>
-      </header>
-      <Layout />
-      <footer>Footer</footer>
-    </>
-  );
-};
-
-export default Home;
-
-const GlobalStyles = createGlobalStyle`
-  body {
+  const GlobalStyles = createGlobalStyle`
+  html,body {
     margin: 0px;
     padding: 0px;
     box-sizing: border-box;
@@ -47,4 +25,28 @@ const GlobalStyles = createGlobalStyle`
   p {
     font-family: 'Lato', sans-serif;
   }
+
+  a {
+    font-family: 'Lato', sans-serif;
+  }
 `;
+
+  return (
+    <>
+      <GlobalStyles />
+      <Head>
+        <title>Nicklas Holmqvist portfolio | Fotograf och utvecklare</title>
+        <meta
+          name="description"
+          content="En portfolio skapad av Nicklas Holmqvist som en frontend utvecklare för att visa fotografier ur projektet Glömd värld i marks härad."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavHeader />
+      <Layout />
+      <footer>Footer</footer>
+    </>
+  );
+};
+
+export default Home;
