eabda741dcc2d671b8bdedc14ff5ffdcdb911d3c
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2022-08-20 09:26:01 +0200

    feat: added project information

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,30 +7,6 @@ import { Layout } from '../components/Layout';
 import { NavHeader } from '../components/NavHeader';
 
 const Home: NextPage = () => {
-  const GlobalStyles = createGlobalStyle`
-  html,body {
-    margin: 0px;
-    padding: 0px;
-    box-sizing: border-box;
-    background-color: #2A2A2A;
-    color: #F0F0F0;
-    font-family: 'Noto Sans', sans-serif;
-  }
-
-  h2 {
-    font-family: 'Alike', serif;
-    font-weight: lighter;
-  }
-
-  p {
-    font-family: 'Lato', sans-serif;
-  }
-
-  a {
-    font-family: 'Lato', sans-serif;
-  }
-`;
-
   return (
     <>
       <GlobalStyles />
@@ -50,3 +26,45 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+const GlobalStyles = createGlobalStyle`
+html,body {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+  background-color: #2A2A2A;
+  color: #F0F0F0;
+  font-family: 'Noto Sans', sans-serif;
+  scroll-behavior: smooth;
+}
+
+h2 {
+  margin: 0px;
+  padding-bottom: 0.5rem;
+  font-size: 1.5rem;
+  font-family: 'Alike', serif;
+  font-weight: 100;
+}
+
+h3 {
+  margin: 0px;
+  padding-bottom: 0.5rem;
+  font-size: 3rem;
+  font-family: 'Lato', sans-serif;
+}
+
+p {
+  margin: 0px;
+  font-family: 'Lato', sans-serif;
+}
+
+a {
+  margin: 0px;
+  font-family: 'Lato', sans-serif;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  font-family: 'Lato', sans-serif;
+}
+`;
