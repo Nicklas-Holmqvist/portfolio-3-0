fc7cdc75c309b82822c7daddc32f27a343edf503
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2022-08-21 18:12:14 +0200

    feat: added galleries

--- components/NavHeader.tsx
+++ components/NavHeader.tsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import React, { useEffect, useState } from 'react';
 
 import { NavLink } from './NavLink';
 
 export interface NavHeader {}
+
+export interface HeaderProps {
+  height: number;
+  active: boolean;
+}
 // Hämta hem från CMS
 const navLinks = ['Projekt', 'Gallerier', 'Om mig'];
 
@@ -25,28 +30,8 @@ export const NavHeader: React.FC<NavHeader> = () => {
     window.addEventListener('scroll', changeBackgroundColor);
   });
 
-  const Header = styled.header`
-    box-sizing: border-box;
-    position: fixed;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    height: ${headerHeight}px;
-    padding: 0 10rem;
-    background-color: ${activeBackgroundColor ? '#2a2a2ada' : 'none'};
-    transition: all 0.5s;
-  `;
-
-  const Nav = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    align-items: end;
-    width: ${navLinks.length * 6}rem;
-  `;
-
   return (
-    <Header>
+    <Header height={headerHeight} active={activeBackgroundColor}>
       <Image src="/logo.png" alt="logo" width={logoSize} height={logoSize} />
       <Nav>
         {navLinks.map((navLink, index) => (
@@ -56,3 +41,41 @@ export const NavHeader: React.FC<NavHeader> = () => {
     </Header>
   );
 };
+
+const Header = styled.header<HeaderProps>`
+  box-sizing: border-box;
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1900px;
+  width: 100%;
+  height: ${(props) => props.height}px;
+  padding: 0 10rem;
+  z-index: 100;
+  ${({ active }) =>
+    active
+      ? css`
+          background-color: #2a2a2ada;
+        `
+      : css`
+          background-color: none;
+        `}
+  transition: all 0.5s;
+  @media (max-width: 1500px) {
+    padding: 0 5rem;
+  }
+  @media (max-width: 1300px) {
+    padding: 0 2rem;
+  }
+  @media (max-width: 1100px) {
+    padding: 0 4rem;
+  }
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  width: ${navLinks.length * 6}rem;
+`;
